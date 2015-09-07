faa110213c984687cf06255b1a7a5340f93da147
Charlie Guo <charlierguo@gmail.com>
2015-09-07 22:43:40 +0000

    Trying to tweak buildpack

--- bin/detect.sh
+++ bin/detect.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # this pack is valid for apps with a manage.py in the root
-if [ -f $1/manage.py ]; then
-  echo "-----> Detected Django manage.py"
-  exit 0
-else
-  echo "FAIL: No Djanago manage.py detected"
+if [ ! -f $1/manage.py ]; then
+  echo "FAIL: No Django manage.py detected"
   exit 1
-fi
+fi 
+
+echo "-----> Detected Django manage.py"
